5d165b94d4b1798eb5b27a7df118a55995258356
jswain24 <jaleesa.swain2@student.uagc.edu>
2025-01-13 21:40:41 -0500

    Create Movieadd.js

---
+++ Movieadd.js
@@ -0,0 +1,17 @@
+import MovieCard from "./MovieCard";
+const MovieList = ({ movies, onEdit, onDelete, onToggleWatched }) => {
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onEdit={onEdit}
+          onDelete={onDelete}
+          onToggleWatched={onToggleWatched}
+        />
+      ))}
+    </div>
+  );
+};
+export default MovieList;
