b1cb05b22088f7e46cd4561becfba95e7405ae1e
jswain24 <jaleesa.swain2@student.uagc.edu>
2025-01-20 18:37:41 -0500

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,13 @@
+Header.js  => to display the title Streamlist
+
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>StreamList</h1>
+        </header>
+    );
+};
+
+export default Header;
