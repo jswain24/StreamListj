2cd104a5179c36ce161327d4dde3add7aa91ce65
jswain24 <jaleesa.swain2@student.uagc.edu>
2025-01-20 19:00:14 -0500

    Update Movieadd.js

--- Movieadd.js
+++ Movieadd.js
@@ -1,17 +1,18 @@
-import MovieCard from "./MovieCard";
-const MovieList = ({ movies, onEdit, onDelete, onToggleWatched }) => {
+import React from "react";
+import ProductCard from "./ProductCard";
+
+const Products = ({ addToCart }) => {
+  const sampleProducts = [
+    { id: 1, title: "Inception", price: 12.99 },
+    { id: 2, title: "Interstellar", price: 14.99 },
+    { id: 3, title: "The Matrix", price: 9.99 },
+  ];
+
   return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onEdit={onEdit}
-          onDelete={onDelete}
-          onToggleWatched={onToggleWatched}
-        />
+    <div className="products">
+      {sampleProducts.map((product) => (
+        <ProductCard key={product.id} product={product} addToCart={addToCart} />
       ))}
     </div>
   );
 };
-export default MovieList;
